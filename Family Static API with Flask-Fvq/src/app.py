947ccfa6906fd8d506bcfa428645d660d39c40d4
Fiorella-vq <fio.89@hotmail.com>
2024-12-21 16:07:34 -0300

    Family Static API with Flask

---
+++ Family Static API with Flask-Fvq/src/app.py
@@ -0,0 +1,69 @@
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
+import os
+from flask import Flask, request, jsonify, url_for
+from flask_cors import CORS
+from utils import APIException, generate_sitemap
+from datastructures import FamilyStructure
+#from models import Person
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+CORS(app)
+
+# create the jackson family object
+jackson_family = FamilyStructure("Jackson")
+
+# Handle/serialize errors like a JSON object
+@app.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
+
+# generate sitemap with all your endpoints
+@app.route('/')
+def sitemap():
+    return generate_sitemap(app)
+
+@app.route('/members', methods=['GET'])
+def handle_hello():
+    # this is how you can use the Family datastructure by calling its methods
+    members = jackson_family.get_all_members()
+    response_body = {
+        "hello": "world",
+        "family": members
+    }
+    return jsonify(members), 200
+
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member_by_id(id):
+    # this is how you can use the Family datastructure by calling its methods
+    member = jackson_family.get_member(id)
+    if member != None:
+        return jsonify(member), 200
+    return jsonify({"msg":"Not found"}), 404
+
+
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    request_body = request.get_json()
+    new_member= jackson_family.add_member(request_body)
+    return jsonify(new_member), 200
+
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member_by_id(id):
+    # this is how you can use the Family datastructure by calling its methods
+    member = jackson_family.delete_member(id)
+    if member != None:
+        return jsonify({"done":True}), 200
+    return jsonify({"msg":"Not found"}), 404
+
+
+
+# this only runs if `$ python src/app.py` is executed
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=True)
---
+++ Family Static API with Flask-Fvq/src/datastructures.py
@@ -0,0 +1,74 @@
+"""
+update this file to implement the following already declared methods:
+- add_member: Should add a member to the self._members list
+- delete_member: Should delete a member from the self._members list
+- update_member: Should update a member from the self._members list
+- get_member: Should return a member from the self._members list
+"""
+from random import randint
+
+class FamilyStructure:
+    def __init__(self, last_name):
+        self.last_name = last_name
+
+        # example list of members
+        self._members = [
+            {
+                "id": self._generateId(),
+                "first_name": "Jhon",
+                "last_name": self.last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "last_name": self.last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "last_name": self.last_name,
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+
+        ]
+
+    # read-only: Use this method to generate random members ID's when adding members into the list
+    def _generateId(self):
+        return randint(0, 99999999)
+
+    def add_member(self, member):
+        # fill this method and update the return
+        new_member={
+            "id": member.get("id", self._generateId()),
+            "first_name": member.get("first_name"),
+            "last_name": self.last_name,
+            "age": member.get("age"),
+            "lucky_numbers": member.get("lucky_numbers"),
+        }
+        self._members.append(new_member)
+        return new_member
+
+
+    def delete_member(self, id):
+        # fill this method and update the return
+        for index in range(0,len(self._members)):
+            if self._members[index].get("id") == id:
+                return self._members.pop(index)
+        return None
+
+
+    def get_member(self, id):
+        # fill this method and update the return
+        for member in self._members:
+            if member.get("id") == id:
+                return member
+        return None        
+
+    # this method is done, it returns a list with all the family members
+    def get_all_members(self):
+        return self._members
